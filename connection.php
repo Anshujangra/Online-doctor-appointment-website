b3ad704136d202ec535b25f4c587b1b438460752
Anshu <149034890+Anshujangra@users.noreply.github.com>
2026-03-30 16:13:09 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,13 +1,10 @@
-<?php
-$servername = "db";
-$username = "root";  
+<?php 
+$servername = "localhost"; 
+$username = "root"; 
 $password = ""; 
 $dbname = "edoc"; 
-
-$database = new mysqli($servername, $username, $password, $dbname);
-
-if ($database->connect_error) {
-    die("Échec de la connexion : " . $database->connect_error);
-}
+$database = new mysqli($servername, $username, $password, $dbname); 
+if ($database->connect_error) { 
+ die("Échec de la connexion : " . $database->connect_error); 
+} 
 ?>
-
